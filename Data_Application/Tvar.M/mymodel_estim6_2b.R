faf5a6600c9ed397c57fcbad5e6c5a4f6511ed82
Ghulam Abdul Qadir <ghulamabdul@gmail.com>
2021-03-30 15:11:05 +0300

    Add files via upload

---
+++ Data_Application/Tvar.M/mymodel_estim6_2b.R
@@ -0,0 +1,616 @@
+
+setwd("/ibex/scratch/qadirga/Project_4/Data_Application/MyModel")
+#setwd("/Users/qadirga/Documents/Project 4-Spacetime modeling/Revisit/Simulation Study/Revisit2/Revisit 3/Data Application/MyModel")
+load("gmrcl_ests.RData")
+rcl.est
+mygm_ini<-rcl.est$par
+rm(list = ls()[-which(ls()=="mygm_ini")])
+load("Myprelimests_oregon.RData")
+#rm(list=ls()[-c(16,19,21,22,27,33,35)])
+library(MASS)
+library(mvtnorm)
+library(fields)
+library(doParallel)
+ncores<-detectCores()
+registerDoParallel(cores = ncores-1)
+getDoParWorkers()
+### functions ####
+
+my.matern<-function(h,a,sigma,nu)
+{
+  h[h==0]<-1e-10
+  num1<-(sigma^2)*(2^(1-nu))/gamma(nu)
+  num2<-(h*a)^nu
+  num3<-besselK(x=(h*a), nu=nu)
+  return(num1*num2*num3)
+}
+
+ls.a<-ls.nu<-ls.sigma<-numeric()
+
+for(i in 1:355)
+{
+  ls.a[i]<-my_exp[[i]]$par[1]
+  ls.nu[i]<-my_exp[[i]]$par[2]
+  ls.sigma[i]<-my_exp[[i]]$par[3]
+}
+
+par(mfrow=c(1,3))
+plot(1:355,ls.a,type="l")
+plot(1:355,ls.nu,type="l")
+plot(1:355,ls.sigma,type="l")
+
+
+
+
+psi<-function(t,a=1,alpha=1,beta=1)
+{
+  temp<-(a*(t^alpha)+1)^beta
+  return(temp)
+}
+
+
+
+##### Creating function for f_alphas #######
+f_alphas<-function(t,alphas,a=1,alpha=1,beta=1,c.p)
+{
+  alpha_mean<-c.p
+  len<-length(alphas)
+  al.creat<-function(a1,a2)
+  {
+    return(((1/(a1^2))+(1/(a2^2)))/2)
+  }
+  d2<-outer(alphas,alphas,al.creat)
+  tmat<-outer(t,t,function(t1,t2) (abs(t1-t2))^2)
+  d1<-psi(t=tmat,a=a,alpha = alpha,beta=beta)/(alpha_mean^2)
+  d3<-matrix(psi(t=0,a=a,alpha = alpha,beta=beta)/(alpha_mean^2),nrow=nrow(d1),ncol=ncol(d1))
+  return(1/sqrt(d1+d2-d3))
+}
+
+
+zeta<-function(t,nus,alphas,a=0.001,alpha=0.9,beta=0.5,d=2,beta2=0.5,c.p)
+{
+  n2<-outer(nus,nus,function(a,b) gamma((a+b)/2))
+  f<-(f_alphas(t=t,alphas = alphas,a=a,alpha=alpha,beta=beta,c.p = c.p))
+  n1<-f^d
+  dig<-(diag(f))^(d/2)
+  #d1<-dig%*%dig
+  d1<-outer(dig,dig,function(a,b) a*b)
+  d2<-outer(nus,nus,function(a,b) sqrt(gamma(a)*gamma(b)))
+  #n2<-outer(alphas,alphas,function(a,b) ((1/a)^(d/4))+((1/b)^(d/4)))
+  #d2<-(f_alphas(t=t,alphas = alphas,a=a,alpha=alpha,beta=beta))^(d/2)
+  tmat<-outer(t,t,function(t1,t2) (abs(t1-t2))^2)
+  d3<-psi(t=tmat,a=a,alpha = alpha,beta=beta2)
+  
+  return((n1*n2)/(d1*d2*d3))
+}
+
+
+
+##### Marginal estimation #####
+library(sp)
+library(gstat)
+library(geoR)
+library(fields)
+library(MASS)
+library(mvtnorm)
+
+############## stepwise MLE ##########
+
+ini.sd<-sd(res.mat)
+
+alpha.func<-function(t, params)
+{
+  temp<-numeric(length(t))
+  for(i in 1:length(t))
+  {
+    temp2<-0
+    for(j in 1:length(params))
+    {
+    
+    temp2<-temp2+t[i]^(j-1)*params[j]
+    
+    }
+    temp[i]<-exp(sum(temp2))
+    
+  }
+  return(temp)
+}
+
+
+nu.func<-function(t, params)
+{
+  temp<-numeric(length(t))
+  for(i in 1:length(t))
+  {
+    temp2<-0
+    for(j in 1:length(params))
+    {
+      
+      temp2<-temp2+t[i]^(j-1)*params[j]
+      
+    }
+    temp[i]<-exp(sum(temp2))
+    
+  }
+  return(temp)
+}
+
+############ Fitting least square model ###########
+##### scaling the time to 0-1 ###
+
+t.points<-1:365
+tseq<-(t.points-(min(t.points)))/(max(t.points)-min(t.points)) ###scaled
+
+tr.days<-355
+alpha.poly.order<-nu.poly.order<-5
+
+xmatrix.alpha<-matrix(NA, nrow = tr.days,ncol = alpha.poly.order-1)
+xmatrix.nu<-matrix(NA, nrow = tr.days,ncol = nu.poly.order-1)
+
+
+for(i in 1:(alpha.poly.order-1))
+{
+  xmatrix.alpha[,i]<-tseq[1:tr.days]^(i)
+}
+
+for(i in 1:(nu.poly.order-1))
+{
+  xmatrix.nu[,i]<-tseq[1:tr.days]^(i)
+}
+
+
+alpha.logy<-log(ls.a)
+nu.logy<-log(ls.nu)
+
+alpha.ldmat<-cbind(alpha.logy,xmatrix.alpha)
+nu.ldmat<-cbind(nu.logy,xmatrix.alpha)
+
+alpha.ldmat<-as.data.frame(alpha.ldmat)
+nu.ldmat<-as.data.frame(nu.ldmat)
+
+
+alpha.lmestim<-lm(alpha.logy~.,data = alpha.ldmat)
+nu.lmestim<-lm(nu.logy~.,data = nu.ldmat)
+par(mfrow=c(1,2))
+plot(tseq[1:tr.days],ls.a,ylab="alpha_s(t)",xlab="t (days)",main="Spatial scale",type="l")
+lines(tseq[1:tr.days],alpha.func(t=tseq[1:tr.days],params = alpha.lmestim$coefficients),col="green")  
+#plot(tseq[1:tr.days],log(alpha.func(t=tseq[1:tr.days],params = alpha.lmestim$coefficients)))
+
+#min(log(alpha.func(t=1:355,params = alpha.lmestim$coefficients)))
+#max(log(alpha.func(t=1:355,params = alpha.lmestim$coefficients)))
+plot(tseq[1:tr.days],ls.nu,ylab="nu_s(t)",xlab="t (days)",main="Spatial smoothness",type="l")
+lines(tseq[1:tr.days],nu.func(t=tseq[1:tr.days],params = nu.lmestim$coefficients),col="green")  
+
+log(ls.nu[1])
+log(ls.a[1])
+log(1.5)
+ini.v<-c(alpha.lmestim$coefficients,nu.lmestim$coefficients,sd(res.mat))
+  
+names(ini.v)<-NULL
+train.dist<-rdist(cbind(my.res.data[,1],my.res.data[,2]))
+nu.func(t=tseq,params=nu.lmestim$coefficients)
+alpha.func(t=tseq,params = alpha.lmestim$coefficients)
+##### Now creating maximum likelihood function ####
+#p<-ini.v
+#dim(res.mat)
+mle.comp.all<-function(tr.dist=train.dist,z=c(res.mat),p,tl=tdays,sl=tr.l,tseq.arg=tseq)
+{
+
+  
+  a.0<-p[1]
+  a.1<-p[2]
+  a.2<-p[3]
+  a.3<-p[4]
+  a.4<-p[5]
+  nu.0<-p[6]
+  nu.1<-p[7]
+  nu.2<-p[8]
+  nu.3<-p[9]
+  nu.4<-p[10]
+  sigma<-p[11]
+  
+  a.s<-alpha.func(t=tseq.arg[1:tl],params = c(a.0,a.1,a.2,a.3,a.4))
+  nu.s<-nu.func(t=tseq.arg[1:tl],params=c(nu.0,nu.1,nu.2,nu.3,nu.4))
+  #a.s<-exp(p[1]+p[2]*(1:355))
+  #nu.s<-exp(p[4]+p[5]*(1:355))
+  
+  loc.n<-nrow(tr.dist)
+  if(sum(c(sigma<=0))!=0)
+  {
+    return(list(mlv=Inf))
+  }
+  else
+  {
+    #myC<-array(dim = c(nrow(tr.dist),ncol(tr.dist),tl))
+    #myC<-foreach(i = tseq.arg[1:tl]) %dopar%
+    #{
+     # my.matern(h=tr.dist,a=a.s[i],nu=nu.s[i],sigma = sigma)
+    #}
+    
+    
+    daywise.nlogl<-foreach(i=1:tl,.packages = c("mvtnorm"))%dopar%
+    {
+      
+      tspec.d<-z[((i-1)*loc.n+1):(i*loc.n)] 
+      #myC<-my.matern(h=tr.dist,a=a.s[i],nu=nu.s[i],sigma = sigma)
+      temp<--dmvnorm(x=tspec.d,mean=(rep(0,times=length(tspec.d))),
+                     sigma =my.matern(h=tr.dist,a=a.s[i],nu=nu.s[i],sigma = sigma) ,log = T,checkSymmetry = F)
+      #rm(myC,tspec.d)
+      
+      temp
+    }
+    #rm(myC)
+    nlogl<-do.call(sum,daywise.nlogl)
+    cat("done computing nlog",p,"\n")
+    return(list(mlv=nlogl))
+  }
+}
+
+
+
+system.time(mle.comp.all(p=ini.v))
+
+
+mle_only_mlv<-function(par)
+  {
+    return(mle.comp.all(p=par)$mlv
+    )
+  }
+  
+mle_only_mlv(ini.v)
+
+optim_marg_loglik <- function(par){
+    optim(par=par,
+          fn = mle_only_mlv,
+          hessian=FALSE,
+          control=list(trace=6,
+                       pgtol=0,
+                       parscale=rep(0.1,length(par)),
+                       maxit=2000))
+
+}
+  
+  
+#myspace.estims<-optim_marg_loglik(ini.v)
+
+#############################################
+#save.image("Oregan_mlep012.RData")
+#load("Oregan_mlep012.RData")
+
+######## Now reporting preliminary estimates ######
+
+#ml1.a<-ml1.nu<-ml1.sigma<-numeric()
+
+#ml1.a<-alpha.func(t=tseq[1:tdays],params = myspace.estims$par[1:3])
+#ml1.nu<-nu.func(t=tseq[1:tdays],params = myspace.estims$par[4:6])
+#ml1.sigma<-myspace.estims$par[7]
+
+
+#plot(tseq[1:tdays],ls.a,type="l",xlab="time",ylab="spatial scale")
+#lines(tseq[1:tdays],ml1.a,col="blue",lwd=2)
+
+
+#plot(tseq[1:tdays],ls.nu,type="l",xlab="time",ylab="spatial smoothness")
+#lines(tseq[1:tdays],ml1.nu,col="blue",lwd=2)
+
+
+
+
+########## Temporal correlation ###########
+
+loc.batch<-list()
+########## Now we do this 10 times ######
+est.rep<-4  ##### No. of repeated subsampling
+nbatch<-46
+set.seed(123)
+loc.batch.ind<-matrix(NA,nrow=tr.l/nbatch,ncol=est.rep*nbatch)
+
+for(i in 1:est.rep)
+{
+  loc.order<-sample(1:tr.l,tr.l)
+  loc.batch[[i]]<-matrix(loc.order,nrow =tr.l/nbatch )
+}
+loc.batch.ind<-do.call(cbind,loc.batch)
+
+
+
+mle.temporal.all<-function(locs=cbind(my.res.data[,1],my.res.data[,2]),myspat1=res.mat,p,t.points=tseq[1:tdays],a.est=ml1.a,nu.est=ml1.nu,sigma.est=ml1.sigma,mysl=length(my.res.data[,1]),mytl=tdays,mynbatch=nbatch,myloc.batch=loc.batch.ind)
+{
+  #distmat<-rdist(locs)
+  mya_bern1<-p[1]
+  myalpha_bern1<-p[2]
+  mybeta_bern1<-p[3]
+  mybeta2_bern1<-p[4]
+  
+  if(sum(c(mya_bern1<=0,mya_bern1<=0,myalpha_bern1<=0,myalpha_bern1>1,mybeta_bern1<0,mybeta_bern1>1,mybeta2_bern1<0))!=0)
+  {
+    return(list(mlv=Inf))
+  }
+  else
+  {
+    
+    
+    myfas.e<-f_alphas(t=t.points,alphas = a.est,a=mya_bern1,alpha=myalpha_bern1,beta=mybeta_bern1,c.p = mean(a.est))
+    
+    myzeta.e<-zeta(t=t.points,nus = nu.est,alphas = a.est,a=mya_bern1,alpha=myalpha_bern1,beta=mybeta_bern1,beta2 =mybeta2_bern1,c.p = mean(a.est) )
+    
+    indic<-ncol(myloc.batch)
+    #myloglv<-0
+    full.dist<-rdist(locs)
+    myloglv<-foreach(sr.batch = 1:indic,.combine = '+',.packages = c("mvtnorm"))%dopar%
+    {
+      mynus.e<-outer(nu.est,nu.est,function(a,b) (a+b)/2)
+      grid=locs
+      spcov.e<-matrix(NA,ncol = mysl*mytl/mynbatch,nrow=mysl*mytl/mynbatch)
+      
+      dist.sp.e<-full.dist[myloc.batch[,sr.batch],myloc.batch[,sr.batch]]
+      locs.e<-grid[myloc.batch[,sr.batch],]
+      sl.sub<-length(locs.e[,1])
+      myspat.e<-myspat1[myloc.batch[,sr.batch],]
+      z<-c(myspat.e)
+      ##### Creating covariance matrix #####
+      for(i in 1:mytl)
+      {
+        for(j in i:mytl)
+        {
+          spcov.e[((i-1)*sl.sub+1):((i-1)*sl.sub+sl.sub),((j-1)*sl.sub+1):((j-1)*sl.sub+sl.sub)]<-spcov.e[((j-1)*sl.sub+1):((j-1)*sl.sub+sl.sub),((i-1)*sl.sub+1):((i-1)*sl.sub+sl.sub)]<-myzeta.e[i,j]*my.matern(h=dist.sp.e,
+                                                                                                                                                                                                                  a=myfas.e[i,j],
+                                                                                                                                                                                                                  nu=mynus.e[i,j],
+                                                                                                                                                                                                                  sigma=sigma.est)
+        }
+      }
+      
+      
+      
+      
+      
+      C<-spcov.e
+      nlogl<--dmvnorm(x=z,mean=(rep(0,times=length(z))),sigma = C,log = T)
+      #myloglv<-myloglv+nlogl
+      #cat("done with ",sr.batch,"\n")
+      nlogl
+    }
+    
+    cat("done with ",p,"\n")
+    return(list(mlv=myloglv))
+  }
+}
+
+#temp.init<-c(10,0.9,0.5,0.5)
+
+
+
+#system.time(mle.temporal.all(p=c(10,0.9,0.5,0.5)))
+
+
+#for(i in 1:10)
+#print(mle.temporal.all(locs = grid,myspat1 =myspat,p=c(mya_bern,myalpha_bern,mybeta_bern),sr.batch = i))
+#system.time(mle.temporal.all(locs = grid,myspat1 =myspat,p=c(mya_bern,myalpha_bern,mybeta_bern),sr.batch = i))
+
+
+#### Finding initial value for temporal correlation ####
+emp.temp.cor<-cor(res.mat)
+image.plot(tseq[1:tdays],tseq[1:tdays],emp.temp.cor)
+temp.ini<-function(p)
+{
+  mya<-p[1]
+  myalpha<-p[2]
+  mybeta<-p[3]
+  mybeta2<-p[4]
+  if(sum(c(mya<=0,myalpha<=0,myalpha>1,mybeta<0,mybeta>1,mybeta2<0))!=0)
+  {
+    return(Inf)
+  }
+  else
+  {
+    t1<-zeta(t=tseq[1:tdays],nus = ml1.nu,alphas = ml1.a,a=mya,alpha=myalpha,beta=mybeta,beta2 =mybeta2,c.p = mean(ml1.a) )
+    return(mean(sum((t1-emp.temp.cor)^2)))
+  }
+}
+#temp.ini(c(mya_bern,myalpha_bern,mybeta_bern))
+set.seed(1)
+ls.ini<-runif(4,min = 0.1,max = 0.9)
+#my.temp.ini<-optim(par=ls.ini,temp.ini,control = list(maxit=1000,trace=6))
+
+#initv.temp<-my.temp.ini$par
+
+
+mle.temp_only_mlv<-function(par)
+{
+  return(mle.temporal.all(p=par)
+         
+  )
+}
+
+
+optim_marg_temp.loglik <- function(par){
+  optim(par=par,
+        fn = mle.temp_only_mlv,
+        hessian=FALSE,
+        control=list(trace=6,
+                     pgtol=0,
+                     parscale=rep(0.1,length(par)),
+                     maxit=2000))
+}
+
+#simkey
+
+#tempporal.est<-optim_marg_temp.loglik(par=initv.temp)
+
+
+#save.image("Mytemp_mlep1.RData")
+
+
+#########################################################
+######### Random composite likelihood ###################
+#########################################################
+
+
+#### Plotting the temporal correlation so far #####
+
+#adhoc_temp<-zeta(t=tseq[1:tdays],nus=ls.nu,alphas = ls.a,a=tempporal.est$par[1],alpha =tempporal.est$par[2],beta = tempporal.est$par[3],
+                # beta2 = tempporal.est$par[4],c.p = mean(ls.a))
+
+#par(mfrow=c(1,2))
+#image.plot(tseq[1:tdays],tseq[1:tdays],emp.temp.cor,xlab="t",ylab="t",main="Empirical")
+#image.plot(tseq[1:tdays],tseq[1:tdays],adhoc_temp,xlab="t",ylab="t",main="Empirical")
+
+
+##############################################################
+
+##### Now we define the random-composite loglikelihood function ######
+
+start.time<-proc.time()
+########## Using composite loglikelihood ###########
+
+names(alpha.lmestim$coefficients)<-NULL
+names(nu.lmestim$coefficients)<-NULL
+
+#exp(0.1)
+my.p1.ini<-c(alpha.lmestim$coefficients,nu.lmestim$coefficients,(mygm_ini[3]))
+#alpha.func(t=tseq[1:tdays],params = c(log(mygm_ini[1]),0,0))
+#nu.func(t=tseq[1:tdays],params = c(log(mygm_ini[2]),0,0))
+my.p2.ini<-c(mygm_ini[4],mygm_ini[5],mygm_ini[6],mygm_ini[7])
+    
+    ####### Composite MLE ######
+    #p=my.comp.ini2
+    
+    ##### Now creating maximum likelihood function ####
+    
+         mle_rcl<-function(locs=cbind(my.res.data[,1],my.res.data[,2]),
+                      myspat1=res.mat,p,t.points=tseq[1:tdays],mysl=length(my.res.data[,1]),
+                      mytl=tdays,mynbatch=nbatch,
+                      myloc.batch=loc.batch.ind,z=c(res.mat),tl=tdays,sl=tr.l,tseq.arg=tseq,tr.dist=train.dist)
+    {
+      
+      
+      #distmat<-rdist(locs)
+      a.0<-p[1]*as.numeric(p[1]>-2.30&p[1]<0.40)-2.30*as.numeric(p[1]<=-2.30)+0.40*as.numeric(p[1]>=0.40)
+      a.1<-p[2]
+      a.2<-p[3]
+      a.3<-p[4]
+      a.4<-p[5]
+      nu.0<-p[6]*as.numeric(p[6]>-1&p[6]<0)-1*as.numeric(p[6]<=-1)+0*as.numeric(p[6]>=0)
+      nu.1<-p[7]
+      nu.2<-p[8]
+      nu.3<-p[9]
+      nu.4<-p[10]
+      sigma<-p[11]
+      mya_bern1<-p[12]
+      myalpha_bern1<-p[13]
+      mybeta_bern1<-p[14]
+      mybeta2_bern1<-p[15]
+      
+      if(sum(c(sigma<=0,mya_bern1<=0,mya_bern1<=0,myalpha_bern1<=0,myalpha_bern1>1,mybeta_bern1<0,mybeta_bern1>1,mybeta2_bern1<0))!=0)
+      {
+        return(list(mlv=Inf))
+      }
+      
+      else{
+        loc.n<-nrow(tr.dist)
+        a.s<-alpha.func(t=tseq.arg[1:tl],params = c(a.0,a.1,a.2,a.3,a.4))
+        nu.s<-nu.func(t=tseq.arg[1:tl],params=c(nu.0,nu.1,nu.2,nu.3,nu.4))
+        
+        
+        daywise.nlogl<-foreach(i=1:tl,.packages = c("mvtnorm"))%dopar%
+          {
+            
+            tspec.d<-z[((i-1)*loc.n+1):(i*loc.n)] 
+            #myC<-my.matern(h=tr.dist,a=a.s[i],nu=nu.s[i],sigma = sigma)
+            temp<--dmvnorm(x=tspec.d,mean=(rep(0,times=length(tspec.d))),
+                           sigma =my.matern(h=tr.dist,a=a.s[i],nu=nu.s[i],sigma = sigma) ,log = T,checkSymmetry = F)
+            #rm(myC,tspec.d)
+            
+            temp
+          }
+        #rm(myC)
+        nlogl1<-do.call(sum,daywise.nlogl)
+        cat("done computing nlogl part1",p,"\n")
+        
+        myfas.e<-f_alphas(t=t.points,alphas = a.s,a=mya_bern1,alpha=myalpha_bern1,beta=mybeta_bern1,c.p = mean(a.s))
+        
+        myzeta.e<-zeta(t=t.points,nus = nu.s,alphas = a.s,a=mya_bern1,alpha=myalpha_bern1,beta=mybeta_bern1,beta2 =mybeta2_bern1,c.p = mean(a.s) )
+        
+        indic<-ncol(myloc.batch)
+        #myloglv<-0
+        full.dist<-rdist(locs)
+        myloglv<-foreach(sr.batch = 1:indic,.combine = '+',.packages = c("mvtnorm"))%dopar%
+          {
+            mynus.e<-outer(nu.s,nu.s,function(a,b) (a+b)/2)
+            grid=locs
+            spcov.e<-matrix(NA,ncol = mysl*mytl/mynbatch,nrow=mysl*mytl/mynbatch)
+            
+            dist.sp.e<-full.dist[myloc.batch[,sr.batch],myloc.batch[,sr.batch]]
+            locs.e<-grid[myloc.batch[,sr.batch],]
+            sl.sub<-length(locs.e[,1])
+            myspat.e<-myspat1[myloc.batch[,sr.batch],]
+            z2<-c(myspat.e)
+            ##### Creating covariance matrix #####
+            for(i in 1:mytl)
+            {
+              for(j in i:mytl)
+              {
+                spcov.e[((i-1)*sl.sub+1):((i-1)*sl.sub+sl.sub),((j-1)*sl.sub+1):((j-1)*sl.sub+sl.sub)]<-spcov.e[((j-1)*sl.sub+1):((j-1)*sl.sub+sl.sub),((i-1)*sl.sub+1):((i-1)*sl.sub+sl.sub)]<-myzeta.e[i,j]*my.matern(h=dist.sp.e,
+                                                                                                                                                                                                                        a=myfas.e[i,j],
+                                                                                                                                                                                                                        nu=mynus.e[i,j],
+                                                                                                                                                                                                                        sigma=sigma)
+              }
+            }
+            
+            
+            
+            
+            
+            C<-spcov.e
+            nlogl2<--dmvnorm(x=z2,mean=(rep(0,times=length(z2))),sigma = C,log = T)
+            #myloglv<-myloglv+nlogl
+            #cat("done with ",sr.batch,"\n")
+            nlogl2
+          }
+        cat("done with nlogl2 ",p,"value=",(nlogl1+myloglv)/2,"\n")
+             return(list(mlv=(nlogl1+myloglv)/2))
+          }
+    
+    } 
+    
+    
+    system.time(mle_rcl(p=c(my.p1.ini,my.p2.ini)))
+    #-201319.3
+    
+    mle.rcl_only_mlv<-function(par)
+    {
+      return(mle_rcl(p=par)
+      )
+    }
+    
+    #log(1)
+    #exp(-2.9)
+    #exp(0.4)
+    myrcl.ini.v<-c(my.p1.ini,my.p2.ini)   
+    #trialp<-c(-1.890971, 19.586, -100.6774, 153.5456, -71.83763, 15.06487, 0.9705362, -8.389941, 15.69335, -8.29979, 0.682874, 20.75965, 0.2311537, 0.4931019, 8.406898e-07)
+    #system.time(mle.rcl_only_mlv(myrcl.ini.v))
+    #mle.rcl_only_mlv(trialp)
+    #mle.rcl_only_mlv(myrcl.ini.v)
+    #trialp-myrcl.ini.v
+    optim_rcl.loglik <- function(par){
+      optim(par=par,
+            fn = mle.rcl_only_mlv,
+            hessian=FALSE,
+            control=list(trace=6,
+                         pgtol=0,
+                         parscale=rep(0.1,length(par)),
+                         maxit=18000))
+    }
+    
+    #simkey
+    
+  rcl.est<-optim_rcl.loglik(par=myrcl.ini.v)
+    
+    save.image("Myrcl_ests_gm_ini6_2b.RData")
+
+
+
+
+
+
+
